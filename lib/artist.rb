6c82603b028e7619680d883f7ec1742ce80ee196
Alex Martinez <alexandr3martin3z@gmail.com>
2019-11-11 02:26:29 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,7 +13,10 @@ class Artist
     Song.all.select {|song|song.artist == self}
   end
 
-  def 
+  def add_song_by_name(name, genre)
+    song = Song.new(name, genre)
+    add_song(song)
+  end
 
 
 
